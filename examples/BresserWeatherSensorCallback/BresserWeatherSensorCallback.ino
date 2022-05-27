00a387109fdf1426c0fcfe1f0b48f65adbf6c76d
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2022-05-27 20:27:49 +0200

    Add files via upload

---
+++ examples/BresserWeatherSensorCallback/BresserWeatherSensorCallback.ino
@@ -0,0 +1,114 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// BresserWeatherSensorCallback.ino
+//
+// Bresser 5-in-1/6-in-1 868 MHz Weather Sensor Radio Receiver 
+// based on CC1101 or SX1276/RFM95W and ESP32/ESP8266
+//
+// https://github.com/matthias-bs/Bresser_Weather_Sensor_Receiver
+//
+// Based on:
+// ---------
+// Bresser5in1-CC1101 by Sean Siford (https://github.com/seaniefs/Bresser5in1-CC1101)
+// RadioLib by Jan Gromeš (https://github.com/jgromes/RadioLib)
+// rtl433 by Benjamin Larsson (https://github.com/merbanan/rtl_433) 
+//     - https://github.com/merbanan/rtl_433/blob/master/src/devices/bresser_5in1.c
+//     - https://github.com/merbanan/rtl_433/blob/master/src/devices/bresser_6in1.c
+//
+// created: 05/2022
+//
+//
+// MIT License
+//
+// Copyright (c) 2022 Matthias Prinke
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// History:
+//
+// 20220523 Created from https://github.com/matthias-bs/Bresser5in1-CC1101
+// 20220524 Moved code to class WeatherSensor
+//
+// ToDo: 
+// - 
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <Arduino.h>
+#include "WeatherSensor.h"
+
+
+WeatherSensor weatherSensor(PIN_RECEIVER_CS, PIN_RECEIVER_IRQ, PIN_RECEIVER_RESET, PIN_RECEIVER_GPIO);
+
+// Example for callback function which is executed while waiting for radio messages
+void loopCallback(void)
+{
+    // Normally something really important would be done here
+    Serial.print(".");
+}
+
+void setup() {    
+    Serial.begin(115200);
+
+    weatherSensor.begin();
+}
+
+
+void loop() 
+{
+    // Attempt to receive entire data set with timeout of 30s and callback function
+    bool decode_ok = weatherSensor.getData(300000, true, &loopCallback);
+    Serial.println();
+    
+    if (decode_ok) {
+        const float METERS_SEC_TO_MPH = 2.237;
+        printf("Id: [%8X] Battery: [%s] ",
+            weatherSensor.sensor_id,
+            weatherSensor.battery_ok ? "OK " : "Low");
+        #ifdef BRESSER_6_IN_1
+            printf("Ch: [%d] ", weatherSensor.chan);
+        #endif
+        if (weatherSensor.temp_ok) {
+            printf("Temp: [%5.1fC] Hum: [%3d%%] ",
+                weatherSensor.temp_c,
+                weatherSensor.humidity);
+        } else {
+            printf("Temp: [---.-C] Hum: [---%%] ");
+        }
+        if (weatherSensor.wind_ok) {
+            printf("Wind max: [%4.1fm/s] Wind avg: [%4.1fm/s] Wind dir: [%5.1fdeg] ",
+                    weatherSensor.wind_gust_meter_sec,
+                    weatherSensor.wind_avg_meter_sec,
+                    weatherSensor.wind_direction_deg);
+        } else {
+            printf("Wind max: [--.-m/s] Wind avg: [--.-m/s] ");
+        }
+        if (weatherSensor.rain_ok) {
+            printf("Rain: [%7.1fmm] ",  
+                weatherSensor.rain_mm);
+        } else {
+            printf("Rain: [-----.-mm] "); 
+        }
+        if (weatherSensor.moisture_ok) {
+            printf("Moisture: [%2d%%]",
+                weatherSensor.moisture);
+        }
+        printf("\n");
+    }
+    delay(100);
+} // loop()

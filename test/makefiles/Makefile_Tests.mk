7e7582188da08611a4ed5c6d3696b88f857026ce
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2022-10-07 10:21:33 +0200

    Add files via upload

---
+++ test/makefiles/Makefile_Tests.mk
@@ -0,0 +1,10 @@
+COMPONENT_NAME=RainGauge
+
+SRC_FILES = \
+  $(PROJECT_SRC_DIR)/RainGauge.cpp \
+
+TEST_SRC_FILES = \
+  $(UNITTEST_SRC_DIR)/TestRainGauge.c \
+  $(UNITTEST_SRC_DIR)/TestRainGaugeReal.c 
+
+include $(CPPUTEST_MAKFILE_INFRA)

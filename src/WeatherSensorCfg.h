45fba75cfb6e007f0d8ebc1b2112270d6c086d82
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2022-10-24 19:28:11 +0200

    Add files via upload

--- src/WeatherSensorCfg.h
+++ src/WeatherSensorCfg.h
@@ -1,12 +1,52 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// WeatherSensor.h
+//
+// Bresser 5-in-1/6-in-1 868 MHz Weather Sensor Radio Receiver 
+// based on CC1101 or SX1276/RFM95W and ESP32/ESP8266
+//
+// https://github.com/matthias-bs/BresserWeatherSensorReceiver
+//
+// NOTE: Application/hardware specific configurations should be made in this file!
+//
+// created: 05/2022
+//
+//
+// MIT License
+//
+// Copyright (c) 2022 Matthias Prinke
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// History:
+//
+//
+// ToDo: 
+// -
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 #if !defined(WEATHER_SENSOR_CFG_H)
 #define WEATHER_SENSOR_CFG_H
 
-// Number of sensors to be received
-#define NUM_SENSORS 2
+#include <Arduino.h>
 
-// Use pinning for LoRaWAN Node 
-#define LORAWAN_NODE
-
+#define NUM_SENSORS     1       // Number of sensors to be received
 
 // List of sensor IDs to be excluded - can be empty
 uint32_t const sensor_ids_exc[] = {};
@@ -17,6 +57,12 @@ uint32_t const sensor_ids_exc[] = {};
 uint32_t const sensor_ids_inc[] = {};
 //uint32_t const sensor_ids_inc[] = { 0x83750871 };
 
+// Use pinning for LoRaWAN Node 
+#define LORAWAN_NODE
+
+// Disable data type which will not be used to save RAM
+#define WIND_DATA_FLOATINGPOINT
+#define WIND_DATA_FIXEDPOINT
 
 // Select appropriate sensor message format(s)
 #define BRESSER_5_IN_1
@@ -26,9 +72,30 @@ uint32_t const sensor_ids_inc[] = {};
 //#define USE_CC1101
 #define USE_SX1276
 
-// Disable data type which will not be used to save RAM
-#define WIND_DATA_FLOATINGPOINT
-#define WIND_DATA_FIXEDPOINT
+#if ( !defined(BRESSER_5_IN_1) && !defined(BRESSER_6_IN_1) )
+    #error "Either BRESSER_5_IN_1 and/or BRESSER_6_IN_1 must be defined!"
+#endif
+
+#if ( defined(USE_CC1101) && defined(USE_SX1276) )
+    #error "Either USE_CC1101 OR USE_SX1276 must be defined!"
+#endif
+
+#if defined(USE_CC1101)
+    #define RECEIVER_CHIP F("[CC1101]")
+#elif defined(USE_SX1276)
+    #define RECEIVER_CHIP F("[SX1276]")
+#else
+    #error "Either USE_CC1101 or USE_SX1276 must be defined!"
+#endif
+
+#define DEBUG_PORT Serial
+#if defined(_DEBUG_MODE_)
+    #define DEBUG_PRINT(...) { DEBUG_PORT.print(__VA_ARGS__); }
+    #define DEBUG_PRINTLN(...) { DEBUG_PORT.println(__VA_ARGS__); }
+#else
+  #define DEBUG_PRINT(...) {}
+  #define DEBUG_PRINTLN(...) {}
+#endif
 
 
 //#define _DEBUG_MODE_

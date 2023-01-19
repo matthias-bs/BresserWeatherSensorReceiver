d3e9779032ef078c6b509987205164f057f7ff22
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2023-01-19 13:23:36 +0100

    Temporarily disabled TestRainGauge

--- test/makefiles/Makefile_Tests.mk
+++ test/makefiles/Makefile_Tests.mk
@@ -4,7 +4,7 @@ SRC_FILES = \
   $(PROJECT_SRC_DIR)/RainGauge.cpp \
 
 TEST_SRC_FILES = \
-  $(UNITTEST_SRC_DIR)/TestRainGauge.c \
-  $(UNITTEST_SRC_DIR)/TestRainGaugeReal.c 
-
+  $(UNITTEST_SRC_DIR)/TestRainGaugeReal.c #\ 
+  #$(UNITTEST_SRC_DIR)/TestRainGauge.c
+  
 include $(CPPUTEST_MAKFILE_INFRA)

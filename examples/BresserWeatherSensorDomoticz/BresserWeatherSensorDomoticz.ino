8a59cd8ccb124433b77d27acb3a3863e6342926e
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2023-01-24 16:54:23 +0100

    Improved WiFi connection robustness

--- examples/BresserWeatherSensorDomoticz/BresserWeatherSensorDomoticz.ino
+++ examples/BresserWeatherSensorDomoticz/BresserWeatherSensorDomoticz.ino
@@ -67,6 +67,7 @@
 // 20221006 Modified secure/non-secure client implementation
 //          Modified string buffer size handling
 // 20221227 Replaced DEBUG_PRINT/DEBUG_PRINTLN by Arduino logging functions
+// 20230124 Improved WiFi connection robustness
 //
 // ToDo:
 // 
@@ -105,6 +106,8 @@
 #define STATUS_INTERVAL 30000   // MQTT status message interval [ms]
 #define DATA_INTERVAL   15000   // MQTT data message interval [ms]
 #define AWAKE_TIMEOUT   300000  // maximum time until sketch is forced to sleep [ms]
+#define WIFI_RETRIES    10      // WiFi connection retries
+#define WIFI_DELAY      1000    // Delay between connection attempts [ms]
 #define SLEEP_INTERVAL  300000  // sleep interval [ms]
 #define SLEEP_EN        false   // enable sleep mode (see notes above!)
 #define USE_SECUREWIFI          // use secure WIFI
@@ -271,6 +274,24 @@ time_t now;
 void publishWeatherdata(void);
 void mqtt_connect(void);
 
+
+//
+// Wait for WiFi connection
+//
+void wifi_wait(int wifi_retries, int wifi_delay)
+{
+    int count = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(wifi_delay);
+        if (++count == wifi_retries) {
+            Serial.printf("WiFi connection timed out, will restart after %d s\n", SLEEP_INTERVAL/1000);
+            ESP.deepSleep(SLEEP_INTERVAL * 1000);
+        }
+    }
+}
+
+
 //
 // Setup WiFi in Station Mode
 //
@@ -281,11 +302,7 @@ void mqtt_setup(void)
     WiFi.hostname(Hostname);
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, pass);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.print(".");
-        delay(1000);
-    }
+    wifi_wait(WIFI_RETRIES, WIFI_DELAY);
     Serial.println(F("connected!"));
     
     #ifdef USE_SECUREWIFI
@@ -339,11 +356,7 @@ void mqtt_setup(void)
 void mqtt_connect(void)
 {
     Serial.print(F("Checking wifi..."));
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.print(".");
-        delay(1000);
-    }
+    wifi_wait(WIFI_RETRIES, WIFI_DELAY);
     
     Serial.print(F("\nMQTT connecting... "));
     while (!client.connect(Hostname, MQTT_USER, MQTT_PASS))

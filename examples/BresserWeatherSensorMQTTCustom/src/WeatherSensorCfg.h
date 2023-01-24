2a39a4a6b2c70db103e9eb10617dec431b3d6f53
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2023-01-24 09:42:22 +0100

    Update WeatherSensorCfg.h

--- examples/BresserWeatherSensorMQTTCustom/src/WeatherSensorCfg.h
+++ examples/BresserWeatherSensorMQTTCustom/src/WeatherSensorCfg.h
@@ -34,7 +34,7 @@
 // SOFTWARE.
 //
 // History:
-//
+// 20230124 Added some default settings based on selected boards in Arduino IDE
 //
 // ToDo: 
 // -
@@ -46,13 +46,86 @@
 
 #include <Arduino.h>
 
-// Replacement for
-// https://github.com/espressif/arduino-esp32/blob/master/cores/esp32/esp32-hal-log.h
-// on ESP8266
+// ------------------------------------------------------------------------------------------------
+// --- Board ---
+// ------------------------------------------------------------------------------------------------
+// Use pinning for LoRaWAN Node 
+
+
+// LILIGO TTGO LoRaP32 board with integrated RF tranceiver (SX1276)
+// See pin definitions in
+// https://github.com/espressif/arduino-esp32/tree/master/variants/ttgo-lora32-*
 //
-// NOTE:
-// DEBUG_ESP_PORT is set in Arduino IDE:
-// Tools->Debug port: "<None>|<Serial>|<Serial1>"
+// This define is set by selecting "TTGO LoRa32-OLED" in the Arduino IDE:
+//#define ARDUINO_TTGO_LoRa32_V1
+
+// Adafruit Feather ESP32S2 with RFM95W "FeatherWing" ADA3232
+// https://github.com/espressif/arduino-esp32/blob/master/variants/adafruit_feather_esp32s2/pins_arduino.h
+//
+// This define is set by selecting "Adafruit Feather ESP32-S2" in the Arduino IDE:
+//#define ADAFRUIT_FEATHER_ESP32S2
+
+// DFRobot Firebeetle32
+// https://github.com/espressif/arduino-esp32/tree/master/variants/firebeetle32/pins_arduino.h
+//
+// This define (not very specific...) is set by selecting "FireBeetle-ESP32" in the Arduino IDE:
+//#define ARDUINO_ESP32_DEV
+
+//#if defined(ARDUINO_TTGO_LoRa32_V1)
+//    #pragma message("ARDUINO_TTGO_LoRa32_V1 defined; using on-board transceiver")
+//    #define USE_SX1276
+//    
+//#elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
+//    #pragma message("ARDUINO_ADAFRUIT_FEATHER_ESP32S2 defined; assuming RFM95W FeatherWing will be used"
+//    #define USE_SX1276
+//    
+//#elif defined(ARDUINO_ESP32_DEV)
+//    #pragma message("Generic ESP32; assuming this is the LoRa_Node board (DFRobot Firebeetle32 + Adafruit RFM95W LoRa Radio)")
+//    #define LORAWAN_NODE
+//    #define USE_SX1276
+//
+//#endif
+
+
+// ------------------------------------------------------------------------------------------------
+// --- Radio Transceiver ---
+// ------------------------------------------------------------------------------------------------
+// Select type of receiver module (if not yet defined based on the assumptions above)
+#if ( !defined(USE_CC1101) && !defined(USE_SX1276) )
+    #define USE_CC1101
+    //#define USE_SX1276
+#endif
+
+
+// ------------------------------------------------------------------------------------------------
+// --- Weather Sensors ---
+// ------------------------------------------------------------------------------------------------
+#define NUM_SENSORS     1       // Number of sensors to be received
+
+// List of sensor IDs to be excluded - can be empty
+#define SENSOR_IDS_EXC {}
+//#define SENSOR_IDS_EXC { 0x39582376 }
+
+// List of sensor IDs to be included - if empty, handle all available sensors
+#define SENSOR_IDS_INC {}
+//#define SENSOR_IDS_INC { 0x83750871 }
+
+
+// ------------------------------------------------------------------------------------------------
+// --- Debug Logging Output ---
+// ------------------------------------------------------------------------------------------------
+// - ESP32:
+//   CORE_DEBUG_LEVEL is set in Adruino IDE:
+//   Tools->Core Debug Level: "<None>|<Error>|<Warning>|<Info>|<Debug>|<Verbose>" 
+//   https://github.com/espressif/arduino-esp32/blob/master/cores/esp32/esp32-hal-log.h
+//
+// - ESP8266:
+//   DEBUG_ESP_PORT is set in Arduino IDE:
+//   Tools->Debug port: "<None>|<Serial>|<Serial1>"
+//
+//   Replacement for
+//   https://github.com/espressif/arduino-esp32/blob/master/cores/esp32/esp32-hal-log.h
+//   on ESP8266:
 #if defined(ESP8266)
     #define ARDUHAL_LOG_LEVEL_NONE      0
     #define ARDUHAL_LOG_LEVEL_ERROR     1
@@ -102,27 +175,6 @@
   #define DEBUG_PRINTLN(...) {}
 #endif
 
-
-#define NUM_SENSORS     1       // Number of sensors to be received
-
-// List of sensor IDs to be excluded - can be empty
-#define SENSOR_IDS_EXC {}
-//#define SENSOR_IDS_EXC { 0x39582376 }
-
-// List of sensor IDs to be included - if empty, handle all available sensors
-#define SENSOR_IDS_INC {}
-//#define SENSOR_IDS_INC { 0x83750871 }
-
-// Use pinning for LoRaWAN Node 
-//#define LORAWAN_NODE
-
-// Use pinning for TTGO ESP32 boards with integrated RF tranceiver (SX1276)
-// https://github.com/espressif/arduino-esp32/tree/master/variants/ttgo-lora32-*
-//#define TTGO_LORA32
-
-// Use pinning for Adafruit Feather ESP32S2 with RFM95W "FeatherWing" ADA3232
-//#define ADAFRUIT_FEATHER_ESP32S2
-
 // Disable data type which will not be used to save RAM
 #define WIND_DATA_FLOATINGPOINT
 #define WIND_DATA_FIXEDPOINT
@@ -130,10 +182,6 @@
 // Select appropriate sensor message format(s)
 #define BRESSER_5_IN_1
 #define BRESSER_6_IN_1
-
-// Select type of receiver module
-#define USE_CC1101
-//#define USE_SX1276
 
 #if ( !defined(BRESSER_5_IN_1) && !defined(BRESSER_6_IN_1) )
     #error "Either BRESSER_5_IN_1 and/or BRESSER_6_IN_1 must be defined!"
@@ -158,6 +206,7 @@
 // ESP8266 D5    D7    D6
 // ESP32   D18   D23   D19
 #if defined(LORAWAN_NODE)
+    // Use pinning for LoRaWAN_Node (https://github.com/matthias-bs/LoRaWAN_Node) 
     #define PIN_RECEIVER_CS   14
     
     // CC1101: GDO0 / RFM95W/SX127x: G0
@@ -168,7 +217,9 @@
     
     // RFM95W/SX127x - GPIOxx / CC1101 - RADIOLIB_NC
     #define PIN_RECEIVER_RST  12
-#elif defined(TTGO_LORA32)
+
+#elif defined(ARDUINO_TTGO_LoRa32_V1)
+    // Use pinning for LILIGY TTGO LoRa32-OLED
     #define PIN_RECEIVER_CS   LORA_CS
     
     // CC1101: GDO0 / RFM95W/SX127x: G0
@@ -180,7 +231,9 @@
     
     // RFM95W/SX127x - GPIOxx / CC1101 - RADIOLIB_NC
     #define PIN_RECEIVER_RST  LORA_RST
+    
 #elif defined(ADAFRUIT_FEATHER_ESP32S2)
+    // Use pinning for Adafruit Feather ESP32S2 with RFM95W "FeatherWing" ADA3232
     #define PIN_RECEIVER_CS   6
     
     // CC1101: GDO0 / RFM95W/SX127x: G0
@@ -191,7 +244,9 @@
     
     // RFM95W/SX127x - GPIOxx / CC1101 - RADIOLIB_NC
     #define PIN_RECEIVER_RST  9
+
 #elif defined(ESP32)
+    // Generic pinning for ESP32 development boards
     #define PIN_RECEIVER_CS   27
     
     // CC1101: GDO0 / RFM95W/SX127x: G0
@@ -204,6 +259,7 @@
     #define PIN_RECEIVER_RST  32
 
 #elif defined(ESP8266)
+    // Generic pinning for ESP8266 development boards (e.g. LOLIN/WEMOS D1 mini)
     #define PIN_RECEIVER_CS   15
     
     // CC1101: GDO0 / RFM95W/SX127x: G0
@@ -215,5 +271,10 @@
     // RFM95W/SX127x - GPIOxx / CC1101 - RADIOLIB_NC
     #define PIN_RECEIVER_RST  2
 #endif
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+#pragma message("Receiver chip: " RECEIVER_CHIP)
+#pragma message("Pin config: RST->" STR(PIN_RECEIVER_RST) ", CS->" STR(PIN_RECEIVER_CS) ", GD0/G0/IRQ->" STR(PIN_RECEIVER_IRQ) ", GDO2/G1/GPIO->" STR(PIN_RECEIVER_GPIO) )
 
 #endif

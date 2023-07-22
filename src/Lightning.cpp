be5e0f16f29b12ceea24d20ef68cc2f51040b4a3
Matthias Prinke <m.prinke@arcor.de>
2023-07-22 18:42:29 +0200

    Added lightning

---
+++ src/Lightning.cpp
@@ -0,0 +1,160 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Lightning.cpp
+//
+// Post-processing of lightning sensor data
+//
+// Input:   
+//     * Timestamp (time and date)
+//     * Sensor startup flag
+//     * Accumulated lightning event counter
+//     * Estimated distance of last strike
+//
+// Output:
+//     * Number of events during last update cycle
+//     * Timestamp of last event
+//     * Number of strikes during past 60 minutes  
+//
+// Non-volatile data is stored in the ESP32's RTC RAM to allow retention during deep sleep mode.
+//
+// https://github.com/matthias-bs/BresserWeatherSensorReceiver
+//
+//
+// created: 07/2023
+//
+//
+// MIT License
+//
+// Copyright (c) 2023 Matthias Prinke
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// History:
+//
+// 20220721 Created
+//
+// ToDo: 
+// - Store non-volatile data in NVS Flash instead of RTC RAM
+//   (to support ESP8266 and to keep data during power-off)
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <Arduino.h>
+#include "Lightning.h"
+
+#ifndef RTC_DATA_ATTR
+   #define RTC_DATA_ATTR static
+#endif
+#if !defined(ESP32)
+   #warning "Lightning with SLEEP_EN only supported on ESP32!"
+#endif
+
+/**
+ * \typedef nvData_t
+ *
+ * \brief Data structure for lightning sensor to be stored in non-volatile memory
+ *
+ * On ESP32, this data is stored in the RTC RAM. 
+ */
+typedef struct {
+    /* Data of last lightning event */
+    uint16_t  prevCount;
+    uint16_t  events;
+    uint8_t   distance;
+    time_t    timestamp;
+
+    /* Data of past 60 minutes */
+    uint16_t  hist[LIGHTNING_HIST_SIZE];
+
+} nvLightning_t;
+
+RTC_DATA_ATTR nvLightning_t nvLightning = {
+   .prevCount = -1,
+   .events = 0,
+   .distance = 0,
+   .timestamp = 0,
+   .hist = {0}
+};
+
+void
+Lightning::reset(void)
+{
+    nvLightning.prevCount      = -1;
+    nvLightning.events         = -1;
+    nvLightning.distance       = 0;
+    nvLightning.timestamp      = 0;
+}
+
+void
+Lightning::init(uint16_t count)
+{
+    for (int i=0; i<LIGHTNING_HIST_SIZE; i++) {
+        nvLightning.hist[i] = count;
+    }
+}
+
+void
+Lightning::update(time_t timestamp, uint16_t count, uint8_t distance, bool startup)
+{
+    if (nvLightning.prevCount == -1) {
+        nvLightning.prevcount = count;
+        return;
+    }
+
+    if (nvLightning.prevCount < count) {
+        nvLightning.prevCount = count;
+        nvLightning.events = count - nvLightning.prevCount;
+        nvLightning.distance = distance;
+        nvLightning.timestamp = timestamp;
+    }
+
+
+    struct tm timeinfo;
+    localtime_r(&timestamp, &timeinfo);
+
+    int min = timeinfo.tm_min;
+    int idx = min / LIGHTNING_UPD_RATE;
+
+    nvLightning.hist[idx] = count;
+}
+
+bool
+Lightning::lastEvent(time_t &timestamp, uint16_t &events, uint8_t &distance)
+{
+    if (nvLightning.events == -1) {
+        return false;
+    }
+
+    events = nvLightning.events;
+    distance = nvLightning.distance;
+    timestamp = nvLightning.timestamp;
+
+    return true;
+}
+
+int
+Lightning::pastHour(time_t timestamp)
+{
+    struct tm timeinfo;
+    localtime_r(&timestamp, &timeinfo);
+    
+    int min = timeinfo.tm_min;
+    int idx = min / LIGHTNING_UPD_RATE;
+
+    return nvLightning.hist[idx] - nvLightning.hist[inc(idx)]; 
+}
---
+++ test/src/TestLightning.cpp
@@ -0,0 +1,104 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// TestLightning.cpp
+//
+// CppUTest unit tests for Lightning - artificial test cases
+//
+// https://github.com/matthias-bs/BresserWeatherSensorReceiver
+//
+//
+// created: 07/2023
+//
+//
+// MIT License
+//
+// Copyright (c) 2023 Matthias Prinke
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// History:
+//
+// 20230722 Created
+//
+// ToDo: 
+// -
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "CppUTest/TestHarness.h"
+
+#include "Lightning.h"
+
+
+
+static Lightning lightning;
+
+/**
+ * \example
+ * struct tm tm;
+ * time_t t;
+ * strptime("6 Dec 2001 12:33:45", "%d %b %Y %H:%M:%S", &tm);
+ * tm.tm_isdst = -1;      // Not set by strptime(); tells mktime()
+ *                        // to determine whether daylight saving time
+ *                        // is in effect
+ * t = mktime(&tm);
+ */
+
+static void setTime(const char *time, tm &tm, time_t &ts)
+{
+  strptime(time, "%Y-%m-%d %H:%M", &tm);
+  ts = mktime(&tm);
+}
+
+TEST_GROUP(TG_LightningBasic) {
+  void setup() {
+      lightning.reset();
+  }
+
+  void teardown() {
+  }
+};
+
+
+/*
+ * Test basic lightning functions
+ */
+TEST(TG_LightningBasic, Test_LightningBasic) {
+  tm        tm;
+  time_t    ts;
+  bool      res;
+  time_t    res_ts;
+  uint16_t  res_events;
+  uint8_t   res_distance;
+  //float     rainSensor;
+
+  printf("< LightningBasic >\n");
+  
+  setTime("2023-07-22 8:00", tm, ts);
+  lightning.update(tm, 48, 5);
+  res = lightning.lastEvent(time_t &res_ts, uint16_t &res_events, uint8_t &res_distance);
+  CHECK_FALSE(res);
+
+  setTime("2022-09-06 8:06", tm, ts);
+  lightning.update(tm, 50, 7);
+  res = lightning.lastEvent(time_t &res_ts, uint16_t &res_events, uint8_t &res_distance);
+  CHECK(res);
+  CHECK_EQUAL(ts, res_ts);
+  CHECK_EQUAL(2, res_events);
+  CHECK_EQUAL(7, res_distance);
+}

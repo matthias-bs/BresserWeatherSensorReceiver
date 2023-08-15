fba628b072957670ff7012f337f6d60794e35247
Matthias Prinke <m.prinke@arcor.de>
2023-08-15 20:22:12 +0200

    Test

--- test/makefiles/Makefile_Tests.mk
+++ test/makefiles/Makefile_Tests.mk
@@ -8,6 +8,5 @@ TEST_SRC_FILES = \
   $(UNITTEST_SRC_DIR)/TestRainGauge.cpp \
   $(UNITTEST_SRC_DIR)/TestLightning.cpp \
   $(UNITTEST_SRC_DIR)/TestRainGaugeReal.cpp
-  #$(UNITTEST_SRC_DIR)/TestRainGauge.c
   
 include $(CPPUTEST_MAKFILE_INFRA)

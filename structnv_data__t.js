a29e2c587bfa54b48b9e2417e5fb5badcc6262ab
matthias-bs <83612361+matthias-bs@users.noreply.github.com>
2024-01-20 11:26:02 +0000

    Deploying to gh-pages from @ matthias-bs/BresserWeatherSensorReceiver@e4cdcc3914c2d6c28392141819153b20605f55de 🚀

--- structnv_data__t.js
+++ structnv_data__t.js
@@ -1,18 +1,14 @@
 var structnv_data__t =
 [
-    [ "head", "structnv_data__t.html#a4622f3ad405485b651469ba2338519a2", null ],
     [ "hist", "structnv_data__t.html#aec7ef2b2900246a5aca2b3992583a687", null ],
     [ "lastUpdate", "structnv_data__t.html#a8b8785eec7e2407de8e8e7c7fa940ee0", null ],
     [ "rainAcc", "structnv_data__t.html#a0d9f87e95625e09f1c30c08f8363354b", null ],
-    [ "rainBuf", "structnv_data__t.html#ae8561abdaa65a8e94e4a773a286f3724", null ],
     [ "rainDayBegin", "structnv_data__t.html#a159baf80e4c8becbe2f6572f1587784a", null ],
     [ "rainMonthBegin", "structnv_data__t.html#a962977b8ab3db5365cf37451e575ddbb", null ],
     [ "rainPreStartup", "structnv_data__t.html#a227808207172128d4bcaa0ea3d516775", null ],
     [ "rainPrev", "structnv_data__t.html#a46bc68954b5c06d06425dc046c3d7b93", null ],
     [ "rainWeekBegin", "structnv_data__t.html#a1ab632cfcd90e5a53bafea97bec3bfe3", null ],
     [ "startupPrev", "structnv_data__t.html#a3052ffde415dee182505cd2b761cc060", null ],
-    [ "tail", "structnv_data__t.html#a488d7b747d7196ebc8de8a9aa7ab6068", null ],
-    [ "tsBuf", "structnv_data__t.html#a05848747a63f4d3219f26e357a2664ee", null ],
     [ "tsDayBegin", "structnv_data__t.html#a1a489817e2d07b8a4b2ad81f68d152db", null ],
     [ "tsMonthBegin", "structnv_data__t.html#a32367cf6c83eb96dfd75bbe61aef1949", null ],
     [ "tsWeekBegin", "structnv_data__t.html#acb364ced4d78e5164859abda99d8eb11", null ],

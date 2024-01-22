d3a906cd44d465322d7cf69f81ac574033b36ba8
Matthias Prinke <m.prinke@arcor.de>
2024-01-22 11:01:51 +0100

    Modified

--- test/header_overrides/Arduino.h
+++ test/header_overrides/Arduino.h
@@ -1,2 +1,7 @@
+#include <string>
 #define RTC_DATA_ATTR static
-#define log_d(...) { printf(__VA_ARGS__); }
+#define String string
+#define log_e(...) { printf(__VA_ARGS__); printf("\n"); }
+#define log_w(...) { printf(__VA_ARGS__); printf("\n"); }
+#define log_d(...) { printf(__VA_ARGS__); printf("\n"); }
+#define log_v(...) { printf(__VA_ARGS__); printf("\n"); }

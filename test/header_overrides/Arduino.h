bf85d08bbe2f1b06765d493d9451cb61ab355e42
Matthias Prinke <m.prinke@arcor.de>
2024-01-22 11:24:41 +0100

    Added WStringMock

--- test/header_overrides/Arduino.h
+++ test/header_overrides/Arduino.h
@@ -1,6 +1,6 @@
-#include <string>
+#include "WstringMock.h"
+
 #define RTC_DATA_ATTR static
-#define String string
 #define log_e(...) { printf(__VA_ARGS__); printf("\n"); }
 #define log_w(...) { printf(__VA_ARGS__); printf("\n"); }
 #define log_d(...) { printf(__VA_ARGS__); printf("\n"); }

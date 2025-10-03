b75ecd5636cfc10ca58c7e3e9192aa8c9be6f45e
Matthias Prinke <m.prinke@arcor.de>
2025-10-03 17:40:21 +0200

    Initial

---
+++ examples/BresserWeatherSensorCanvasGauges/secrets.h
@@ -0,0 +1,7 @@
+// Replace by your external WiFi access point's credentials
+#define WIFI_SSID "MY_SSID"
+#define WIFI_PASSWORD "MY_PASSWORD"
+
+// Define your ESP access point's credentials
+#define WIFI_AP_SSID "AP-WeatherDashboard";
+#define WIFI_AP_PASSWORD "0123456789";

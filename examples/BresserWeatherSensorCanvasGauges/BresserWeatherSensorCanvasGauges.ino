396398db9e0d6f94a8766fafc310f7c9851e7a19
Matthias Prinke <m.prinke@arcor.de>
2025-10-04 16:59:16 +0200

    Added ESP8266 compatibility

--- examples/BresserWeatherSensorCanvasGauges/BresserWeatherSensorCanvasGauges.ino
+++ examples/BresserWeatherSensorCanvasGauges/BresserWeatherSensorCanvasGauges.ino
@@ -62,14 +62,19 @@
 // To Do:
 // - Improved page layout
 // - SSL/TLS support (optional)
-// - ESP8266 support (optional)
 //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <Arduino.h>
+#if defined(ARDUINO_ARCH_ESP32)
 #include <WiFi.h>
 #include <ESPmDNS.h>
-#include <AsyncTCP.h>          // https://github.com/ESP32Async/AsyncTCP
+#include <AsyncTCP.h> // https://github.com/ESP32Async/AsyncTCP
+#else
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <ESPAsyncTCP.h>
+#endif
 #include <ESPAsyncWebServer.h> // https://github.com/ESP32Async/ESPAsyncWebServer
 #include <time.h>
 #include <LittleFS.h>
@@ -81,6 +86,13 @@
 
 #define MAX_SENSORS 2
 #define RX_TIMEOUT 180000 // sensor receive timeout [ms]
+
+#if defined(ARDUINO_ARCH_ESP32)
+const uint8_t KEY_RAINGAUGE_RESET = KEY_BUILTIN;
+#else
+// Check if this GPIO pin is available on your board
+const uint8_t KEY_RAINGAUGE_RESET = D0;
+#endif
 
 // Stop reception when data of at least one sensor is complete
 // #define RX_FLAGS DATA_COMPLETE
@@ -219,6 +231,8 @@ void initWiFi()
 #endif
 }
 
+
+// Send ping message during weather sensor reception
 void sendPing()
 {
   static unsigned long lastTime;
@@ -230,15 +244,46 @@ void sendPing()
   yield();
 }
 
+
+// Helper in place of getLocalTime usage (ESP32/ESP8266 compatibility)
+bool getLocalTimeCompat(struct tm *info, uint32_t timeout_ms = 2000)
+{
+  uint32_t start = millis();
+  while (millis() - start < timeout_ms)
+  {
+    time_t now = time(nullptr);
+    if (now > 100000)
+    { // NTP synced
+#if defined(ARDUINO_ARCH_ESP8266)
+      struct tm *tmp = localtime(&now);
+      if (!tmp)
+      {
+        delay(10);
+        continue;
+      }
+      *info = *tmp;
+#else
+      localtime_r(&now, info);
+#endif
+      return true;
+    }
+    delay(10);
+  }
+  return false;
+}
+
+// Print local time
 void printLocalTime()
 {
   struct tm timeinfo;
-  if (!getLocalTime(&timeinfo))
+  if (!getLocalTimeCompat(&timeinfo))
   {
     Serial.println("Failed to obtain time");
     return;
   }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  char buf[64];
+  strftime(buf, sizeof(buf), "%A, %B %d %Y %H:%M:%S", &timeinfo);
+  Serial.println(buf);
 }
 
 void setup()
@@ -246,9 +291,9 @@ void setup()
   // Serial port for debugging purposes
   Serial.begin(115200);
 
-  pinMode(KEY_BUILTIN, INPUT);
+  pinMode(KEY_RAINGAUGE_RESET, INPUT);
 
-  if (digitalRead(KEY_BUILTIN) == LOW)
+  if (digitalRead(KEY_RAINGAUGE_RESET) == LOW)
   {
     log_i("Resetting rain gauge data");
     rainGauge.reset();

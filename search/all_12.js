b9e5679147fa8f4fb31424a685cd6a603024b81c
matthias-bs <83612361+matthias-bs@users.noreply.github.com>
2026-02-03 02:49:18 +0000

    Deploying to gh-pages from @ matthias-bs/BresserWeatherSensorReceiver@9ab8898dbcdc54c414209d8143c32d60dafea56a 🚀

--- search/all_12.js
+++ search/all_12.js
@@ -2,5 +2,11 @@ var searchData=
 [
   ['weather_0',['Weather',['../struct_weather_sensor_1_1_weather.html',1,'WeatherSensor']]],
   ['weathersensor_1',['WeatherSensor',['../class_weather_sensor.html',1,'']]],
-  ['wind_5fok_2',['wind_ok',['../struct_weather_sensor_1_1_weather.html#aa3808f22671713aa03bf42aa0cc86e9d',1,'WeatherSensor::Weather']]]
+  ['wind_5favg_5fmeter_5fsec_2',['wind_avg_meter_sec',['../struct_weather_sensor_1_1_weather.html#ae97ca7f51a6cf9d490a12a1993aa846a',1,'WeatherSensor::Weather']]],
+  ['wind_5favg_5fmeter_5fsec_5ffp1_3',['wind_avg_meter_sec_fp1',['../struct_weather_sensor_1_1_weather.html#a69d2abf3cc612c59d1696cb204f05ef3',1,'WeatherSensor::Weather']]],
+  ['wind_5fdirection_5fdeg_4',['wind_direction_deg',['../struct_weather_sensor_1_1_weather.html#a2549b2dcd4b65aeedb4af87b32874b51',1,'WeatherSensor::Weather']]],
+  ['wind_5fdirection_5fdeg_5ffp1_5',['wind_direction_deg_fp1',['../struct_weather_sensor_1_1_weather.html#ab4c2aea8c34f258e683c19e6398e4501',1,'WeatherSensor::Weather']]],
+  ['wind_5fgust_5fmeter_5fsec_6',['wind_gust_meter_sec',['../struct_weather_sensor_1_1_weather.html#adf0cd1deda13205b98c6d7937e236d05',1,'WeatherSensor::Weather']]],
+  ['wind_5fgust_5fmeter_5fsec_5ffp1_7',['wind_gust_meter_sec_fp1',['../struct_weather_sensor_1_1_weather.html#a4fdbd9927bf9394da858b5e5b99f345c',1,'WeatherSensor::Weather']]],
+  ['wind_5fok_8',['wind_ok',['../struct_weather_sensor_1_1_weather.html#aa3808f22671713aa03bf42aa0cc86e9d',1,'WeatherSensor::Weather']]]
 ];
--- struct_weather_sensor_1_1_weather.js
+++ struct_weather_sensor_1_1_weather.js
@@ -13,5 +13,11 @@ var struct_weather_sensor_1_1_weather =
     [ "tglobe_ok", "struct_weather_sensor_1_1_weather.html#af507c720368ff483c11e1baf9ef62904", null ],
     [ "uv", "struct_weather_sensor_1_1_weather.html#a8b9670aa7c3219426a2a319ce2e1d75e", null ],
     [ "uv_ok", "struct_weather_sensor_1_1_weather.html#abb7e6eac7b062270ec7e2bbcb7d1d0f5", null ],
+    [ "wind_avg_meter_sec", "struct_weather_sensor_1_1_weather.html#ae97ca7f51a6cf9d490a12a1993aa846a", null ],
+    [ "wind_avg_meter_sec_fp1", "struct_weather_sensor_1_1_weather.html#a69d2abf3cc612c59d1696cb204f05ef3", null ],
+    [ "wind_direction_deg", "struct_weather_sensor_1_1_weather.html#a2549b2dcd4b65aeedb4af87b32874b51", null ],
+    [ "wind_direction_deg_fp1", "struct_weather_sensor_1_1_weather.html#ab4c2aea8c34f258e683c19e6398e4501", null ],
+    [ "wind_gust_meter_sec", "struct_weather_sensor_1_1_weather.html#adf0cd1deda13205b98c6d7937e236d05", null ],
+    [ "wind_gust_meter_sec_fp1", "struct_weather_sensor_1_1_weather.html#a4fdbd9927bf9394da858b5e5b99f345c", null ],
     [ "wind_ok", "struct_weather_sensor_1_1_weather.html#aa3808f22671713aa03bf42aa0cc86e9d", null ]
 ];

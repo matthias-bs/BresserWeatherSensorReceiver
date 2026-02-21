6d93943ce0f7affe132c1c0475cfe8efe604c280
Matthias Prinke <m.prinke@arcor.de>
2026-02-21 14:01:08 +0100

    Update Makefile_Tests.mk to include MQTT communication test files

--- test/makefiles/Makefile_Tests.mk
+++ test/makefiles/Makefile_Tests.mk
@@ -3,7 +3,8 @@ COMPONENT_NAME=RainGauge
 SRC_FILES = \
   $(PROJECT_SRC_DIR)/RainGauge.cpp \
   $(PROJECT_SRC_DIR)/Lightning.cpp \
-  $(PROJECT_SRC_DIR)/WeatherUtils.cpp
+  $(PROJECT_SRC_DIR)/WeatherUtils.cpp \
+  $(PROJECT_SRC_DIR)/../examples/BresserWeatherSensorMQTT/src/mqtt_comm.cpp
 
 MOCKS_SRC_DIRS = \
   $(UNITTEST_ROOT)/mocks
@@ -11,7 +12,8 @@ MOCKS_SRC_DIRS = \
 TEST_SRC_FILES = \
   $(UNITTEST_SRC_DIR)/TestRainGauge.cpp \
   $(UNITTEST_SRC_DIR)/TestLightning.cpp \
-  $(UNITTEST_SRC_DIR)/TestWeatherUtils.cpp
-  #$(UNITTEST_SRC_DIR)/TestRainGaugeReal.cpp
+  $(UNITTEST_SRC_DIR)/TestWeatherUtils.cpp \
+  $(UNITTEST_SRC_DIR)/TestMQTTComm.cpp
+  #$(UNITTEST_SRC_DIR)/TestRainGaugeReal.cpp  
   
 include $(CPPUTEST_MAKFILE_INFRA)

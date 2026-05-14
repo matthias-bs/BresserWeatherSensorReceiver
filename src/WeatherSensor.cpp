98e804755449b0be45cc603ae37ffce120332ca8
Matthias Prinke <m.prinke@arcor.de>
2026-05-14 22:43:07 +0200

    Add support for Heltec WiFi LoRa 32(V4) with pin definitions and RF switch configuration

--- src/WeatherSensor.cpp
+++ src/WeatherSensor.cpp
@@ -114,6 +114,7 @@
 // 20260309 Fixed Static Initialization Order Fiasco (SIOF) for SPIClass on LilyGo T3S3 boards
 // 20260310 Added support for selecting SPI bus on ESP32 boards
 // 20260611 Added pin definitions for Heltec Wireless Stick Lite V3 (SX1262)
+// 20260514 Added RF switch and TCXO configuration for Heltec WiFi LoRa 32(V4)
 //
 // ToDo:
 // -
@@ -126,7 +127,7 @@
 namespace WeatherSensorReceiver
 {
 #if defined(ARDUINO_LILYGO_T3S3_SX1262) || defined(ARDUINO_LILYGO_T3S3_SX1276) || defined(ARDUINO_LILYGO_T3S3_LR1121) || \
-    defined(HELTEC_WIRELESS_STICK_LITE_V3)
+    defined(HELTEC_WIRELESS_STICK_LITE_V3) || defined(ARDUINO_HELTEC_WIFI_LORA_32_V4)
     // Use a statically allocated SPIClass with the integer bus-number constructor instead of
     // copying the global SPI object, to avoid a Static Initialization Order Fiasco (SIOF):
     // the compiler-generated copy constructor copies paramLock from SPI, which may be NULL
@@ -141,7 +142,7 @@ namespace WeatherSensorReceiver
 #pragma message("Using CC1101 radio module")
     RADIO_CHIP radio = new Module(PIN_RECEIVER_CS, PIN_RECEIVER_IRQ, RADIOLIB_NC, PIN_RECEIVER_GPIO);
 #elif defined(ARDUINO_LILYGO_T3S3_SX1262) || defined(ARDUINO_LILYGO_T3S3_SX1276) || defined(ARDUINO_LILYGO_T3S3_LR1121) || \
-      defined(HELTEC_WIRELESS_STICK_LITE_V3) || defined(LORA_SPI_BUS)
+      defined(HELTEC_WIRELESS_STICK_LITE_V3) || defined(ARDUINO_HELTEC_WIFI_LORA_32_V4) || defined(LORA_SPI_BUS)
     RADIO_CHIP radio = new Module(PIN_RECEIVER_CS, PIN_RECEIVER_IRQ, PIN_RECEIVER_RST, PIN_RECEIVER_GPIO, spi);
 #else
     RADIO_CHIP radio = new Module(PIN_RECEIVER_CS, PIN_RECEIVER_IRQ, PIN_RECEIVER_RST, PIN_RECEIVER_GPIO);
@@ -204,7 +205,7 @@ int16_t WeatherSensor::begin(uint8_t max_sensors_default, bool init_filters, dou
     }
 
 #if defined(ARDUINO_LILYGO_T3S3_SX1262) || defined(ARDUINO_LILYGO_T3S3_SX1276) || defined(ARDUINO_LILYGO_T3S3_LR1121) || \
-    defined(HELTEC_WIRELESS_STICK_LITE_V3) || defined(LORA_SPI_BUS)
+    defined(HELTEC_WIRELESS_STICK_LITE_V3) || defined(ARDUINO_HELTEC_WIFI_LORA_32_V4) || defined(LORA_SPI_BUS)
     spi.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
 #endif
 
@@ -252,7 +253,7 @@ int16_t WeatherSensor::begin(uint8_t max_sensors_default, bool init_filters, dou
     radio.setTCXO(1.7);
 #endif
 
-#if defined(HELTEC_WIRELESS_STICK_LITE_V3)
+#if defined(HELTEC_WIRELESS_STICK_LITE_V3) || defined(ARDUINO_HELTEC_WIFI_LORA_32_V4)
     // RF switch is controlled internally by SX1262 DIO2
     // (SX126X_DIO2_AS_RF_SWITCH in Meshtastic heltec_wsl_v3/variant.h)
     radio.setDio2AsRfSwitch(true);
--- src/WeatherSensorCfg.h
+++ src/WeatherSensorCfg.h
@@ -71,6 +71,7 @@
 // 20241227 Improved maintainability of board definitions
 // 20260114 Added pin definitions for Seeed Studio XIAO ESP32S3 with Wio-SX1262
 // 20260611 Added pin definitions for Heltec Wireless Stick Lite V3 (SX1262)
+// 20260514 Added pin definitions for Heltec WiFi LoRa 32(V4)
 //
 // ToDo:
 // -
@@ -176,6 +177,9 @@
 
 // This define is set by selecting "Board: Heltec WiFi LoRa 32(V2)" in the Arduino IDE:
 //#define ARDUINO_HELTEC_WIFI_LORA_32_V2
+
+// This define is set by selecting "Board: Heltec WiFi LoRa 32(V4)" in the Arduino IDE:
+//#define ARDUINO_HELTEC_WIFI_LORA_32_V4
 
 // Adafruit Feather ESP32S2 with RFM95W "FeatherWing" ADA3232
 // https://github.com/espressif/arduino-esp32/blob/master/variants/adafruit_feather_esp32s2/pins_arduino.h
@@ -307,6 +311,19 @@
     #pragma message("ARDUINO_HELTEC_WIFI_LORA_32_V3 / ARDUINO_HELTEC_VISION_MASTER_T190 defined; using on-board transceiver")
     #define USE_SX1262
     #define PIN_RECEIVER_CS   SS
+    #define PIN_RECEIVER_IRQ  DIO0
+    #define PIN_RECEIVER_GPIO BUSY_LoRa
+    #define PIN_RECEIVER_RST  RST_LoRa
+
+#elif defined(ARDUINO_HELTEC_WIFI_LORA_32_V4)
+    // Heltec WiFi LoRa 32(V4)
+    #pragma message("ARDUINO_HELTEC_WIFI_LORA_32_V4 defined; using on-board transceiver")
+    #define USE_SX1262
+    #define LORA_CS           8
+    #define LORA_SCK          9
+    #define LORA_MOSI         10
+    #define LORA_MISO         11
+    #define PIN_RECEIVER_CS   LORA_CS
     #define PIN_RECEIVER_IRQ  DIO0
     #define PIN_RECEIVER_GPIO BUSY_LoRa
     #define PIN_RECEIVER_RST  RST_LoRa
